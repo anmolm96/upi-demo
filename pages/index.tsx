68e424dadbfb6012ef79c05a2d2737c0dd3d588f
anmolm96 <anmol.maini@gmail.com>
2022-05-30 12:58:30 -0500

    Disable gray color

--- pages/index.tsx
+++ pages/index.tsx
@@ -76,7 +76,7 @@ const Home: NextPage = () => {
               />
             </div>
             <button
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded text-sm disabled:cursor-not-allowed"
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded text-sm disabled:cursor-not-allowed disabled:bg-gray-400"
               disabled={upi.name === "" || upi.vpa === ""}
             >
               Get ₹1
